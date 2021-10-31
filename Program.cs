9fbc1b1473604b4fa3f0ba54a8e3b80cdd1f4cb1
uwmkzacharewicz <75528340+uwmkzacharewicz@users.noreply.github.com>
2021-10-31 20:19:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // menu zwykłe:
+            //MojeMenu.StartMojeMenu();
+            
+            // menu kolorowe:
+            Menu.StartMenu();
+        }
+    }
+}
