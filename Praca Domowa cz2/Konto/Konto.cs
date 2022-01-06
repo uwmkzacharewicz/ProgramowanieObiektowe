391f3a8d421f23d684de3f98d6736ec3d359b7be
uwmkzacharewicz <karolzach87@gmail.com>
2022-01-06 18:28:45 +0100

    Konto

---
+++ Praca Domowa cz2/Konto/Konto.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace PO_pracaDomowaGrupa2
+{    
+    enum TypOperacji
+    {
+        KasaWplata,
+        KasaWyplata,
+        BankomatWplata,
+        BankomatWyplata,
+        PrzelewZlecono,
+        PrzelewOtrzymano,
+        KartaPlatnosc
+    }
+    class Konto
+    {
+        private List<string> ListaOperacji = new List<string>();
+
+        public Konto(string wlasciciel, decimal stanKonta)
+        {
+            WlascicielKonta = wlasciciel;
+            StanKonta = stanKonta;
+            SaldoPoczatkowe = stanKonta;
+            SaldoKoncowe = stanKonta;
+            ID_Klient = NextKlientID;
+            NextKlientID++;
+        }
+        public decimal SaldoPoczatkowe { get; set; }
+        public decimal SaldoKoncowe { get; set; }
+        public string WlascicielKonta { get; set; }
+        public decimal StanKonta { get; set; }
+        public int ID_Klient { get; private set; }
+
+        private static int NextKlientID = 1000;
+
+
+        public void WyswietlWyciag()
+        {
+            string path = ID_Klient + ".txt";
+
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Brak operacji na koncie");
+                return;
+            }
+
+            StreamReader reader = File.OpenText(path);
+            string str = "";
+
+            while ((str = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(str);
+            }
+            reader.Close();
+        }
+
+        public void GenerujWyciag()
+        {
+            string path = ID_Klient + ".txt";
+            StreamWriter writer = File.CreateText(path);
+
+            //writer = File.CreateText(path);
+            writer.WriteLine(String.Concat(Enumerable.Repeat("-", 80)));
+            writer.WriteLine($"Właściciel: {WlascicielKonta}        Identyfikator klienta: {ID_Klient}");
+            writer.WriteLine($"Saldo początkowe: {SaldoPoczatkowe,-10:C2}");
+            writer.WriteLine($"Saldo końcowe: {SaldoKoncowe,-10:C2}");
+            writer.WriteLine();
+
+            writer.WriteLine(String.Concat(Enumerable.Repeat("-", 80)));
+            writer.WriteLine();
+            writer.WriteLine("     OPERACJA                             KWOTA              SALDO     ");
+            writer.WriteLine();
+            writer.WriteLine(String.Concat(Enumerable.Repeat("-", 80)));
+
+            //writer = new StreamWriter(path, true);           
+
+
+            if (ListaOperacji.Count == 0)
+            {
+                writer.WriteLine("Brak operacji na koncie");
+            }
+            else
+            {
+                foreach (string operacja in ListaOperacji)
+                {
+                    writer.WriteLine(operacja);
+                }
+            }
+
+            writer.Close();
+        }
+
+        public void OperacjaNaRachunku(string zleceniodawna, decimal kwota, TypOperacji typ)
+        {
+            string nazwaOperacji = SprawdzOperacje(typ);
+
+            if (typ == TypOperacji.BankomatWplata || typ == TypOperacji.KasaWplata || typ == TypOperacji.PrzelewOtrzymano)
+            {
+                StanKonta += kwota;
+            }
+            else
+            {
+                if (kwota > StanKonta)
+                {
+                    Console.WriteLine("Brak środków na koncie. Transakcja odrzucona\n");
+                    return ;
+                }
+                StanKonta += (-1)*kwota;
+            }
+
+            StworzOperacje(nazwaOperacji, zleceniodawna, kwota);
+
+            //string operacja, opis;
+            //operacja = $"{DateTime.Now}\n";
+            //opis = $"[{ID_Klient}] {nazwaOperacji, -30}" + $"kwota: {kwota, -20:C2}"  + $"Saldo po operacji: {StanKonta,-20:C2}\n";
+            //operacja += $"{nazwaOperacji, -30}" + $"{kwota, 20:C2}"  + $"{StanKonta, 20:C2}\n";
+            //operacja += $"{zleceniodawna}\n";
+            //operacja += String.Concat(Enumerable.Repeat("-", 80));
+
+            //SaldoKoncowe = StanKonta;
+            //Console.WriteLine($"Saldo końcowe: {SaldoKoncowe}");
+
+            ////_out = $"{DateTime.Now} Przelew do {Odbiorca}: {kwota:C2}, " + String.Concat(Enumerable.Repeat("-", 20)) + $" saldo po operacji: {StanKonta:C2}";
+            //Console.WriteLine(opis);
+            //ListaOperacji.Add(operacja);
+ 
+        }
+
+        public void StworzOperacje(string nazwaoperacji, string zleceniodawca, decimal kwota)
+        {
+            string operacja, opis;
+            operacja = $"{DateTime.Now}\n";
+            opis = $"[{ID_Klient}] {nazwaoperacji,-30}" + $"kwota: {kwota,-20:C2}" + $"Saldo po operacji: {StanKonta,-20:C2}\n";
+            operacja += $"{nazwaoperacji,-30}" + $"{kwota,20:C2}" + $"{StanKonta,20:C2}\n";
+            operacja += $"{zleceniodawca}\n";
+            operacja += String.Concat(Enumerable.Repeat("-", 80));
+
+            SaldoKoncowe = StanKonta;
+            Console.WriteLine($"Saldo końcowe: {SaldoKoncowe}");
+            Console.WriteLine(opis);
+            ListaOperacji.Add(operacja);
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"[{ID_Klient}] {WlascicielKonta}, stan konta: {StanKonta}";
+        }
+
+        static string SprawdzOperacje(TypOperacji typ)
+        {
+            string operacja ="";
+
+            switch (typ)
+            {
+                case TypOperacji.KasaWplata:
+                    {
+                        operacja = "WPŁATA W KASIE BANKU";
+                        break;
+                    }
+                case TypOperacji.KasaWyplata:
+                    {
+                        operacja = "WYPŁATA W KASIE BANKU";
+                        break;
+                    }
+                case TypOperacji.PrzelewZlecono:
+                    {
+                        operacja = "PRZELEW WYCHODZACY";
+                        break;
+                    }
+                case TypOperacji.PrzelewOtrzymano:
+                    {
+                        operacja = "PRZELEW KRAJOWY";
+                        break;
+                    }
+                case TypOperacji.KartaPlatnosc:
+                    {
+                        operacja = "TRANSAKCJA KARTĄ DEBETOWĄ";
+                        break;
+                    }
+                case TypOperacji.BankomatWplata:
+                    {
+                        operacja = "WPŁATA BANKOMAT";
+                        break;
+                    }
+                case TypOperacji.BankomatWyplata:
+                    {
+                        operacja = "WYPŁATA Z BANKOMATU";
+                        break;
+                    }
+            }
+
+            return operacja;
+        }
+   
+     
+
+    }
+}
---
+++ Praca Domowa cz2/Konto/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace PO_pracaDomowaGrupa2
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Konto osoba1 = new Konto("Jan Kowal", 1000);
+            Konto osoba2 = new Konto("Janina Nowak", 0);
+            Konto osoba3 = new Konto("Marian Kwiatek", 6000);
+            Konto osoba4 = new Konto("Julian Wesoły", 0);
+
+            Console.WriteLine("\n\nOsoba 1: Operacje na koncie: \n");
+            osoba1.OperacjaNaRachunku("Właściciel", 1000, TypOperacji.BankomatWplata);
+            osoba1.OperacjaNaRachunku("Właściciel", 500, TypOperacji.KartaPlatnosc);
+            osoba1.OperacjaNaRachunku("Jan Nowak", 1200, TypOperacji.PrzelewZlecono);
+            osoba1.OperacjaNaRachunku("Właściciel", 1200, TypOperacji.KasaWyplata);
+            osoba1.OperacjaNaRachunku("Właściciel", 1500, TypOperacji.BankomatWplata);
+
+            Console.WriteLine("\n\nGenerowanie wyciągu: \n");
+            osoba1.GenerujWyciag();
+            osoba1.WyswietlWyciag();
+
+            Console.WriteLine("\n\nOsoba 2: Operacje na koncie: \n");
+            osoba2.OperacjaNaRachunku("Właściciel", 2000, TypOperacji.KasaWplata);
+            osoba2.OperacjaNaRachunku("Sklep ABC", 125.5m, TypOperacji.KartaPlatnosc);
+            osoba2.OperacjaNaRachunku("Sklep 'Mechanik'", 250.75m, TypOperacji.KartaPlatnosc);
+
+            Console.WriteLine("\n\nGenerowanie wyciągu: \n");
+            osoba2.GenerujWyciag();
+            osoba2.WyswietlWyciag();
+
+
+            Console.WriteLine("\n\nOsoba 3: Operacje na koncie: \n");
+            osoba3.OperacjaNaRachunku("Właściciel", 2500, TypOperacji.BankomatWplata);
+            osoba3.OperacjaNaRachunku("Sklep RTV/AGD", 2700, TypOperacji.KartaPlatnosc);
+            osoba3.OperacjaNaRachunku("Halina Nowak", 1200, TypOperacji.PrzelewZlecono);
+            osoba3.OperacjaNaRachunku("Mariola Kwiatek", 1500, TypOperacji.PrzelewOtrzymano);
+            osoba3.OperacjaNaRachunku("Właściciel", 2500, TypOperacji.KasaWyplata);
+            Console.WriteLine("\n\nGenerowanie wyciągu: \n");
+            osoba3.GenerujWyciag();
+            osoba3.WyswietlWyciag();
+
+
+            Console.WriteLine("\n\nOsoba 4: Operacje na koncie: \n");
+            osoba4.OperacjaNaRachunku("Właściciel", 1200, TypOperacji.BankomatWyplata);
+            Console.WriteLine("\n\nGenerowanie wyciągu: \n");
+            osoba4.GenerujWyciag();
+            osoba4.WyswietlWyciag();
+
+
+
+
+
+
+
+
+        }
+    }
+}
